969bcff4a43d396a4c2ed77f7d98c02231c01f26
pankeku <drwagw@gmail.com>
2022-07-25 18:10:05 +0300

    Try to implement minimax

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
-  let board = ['', '', '', '', '', '', '', '', ''];
+  //let board = ['', '', '', '', '', '', '', '', ''];
+  let board = ['X', 'O', 'X', '', '', '', '', 'O', ''];
 
   const getBoard = () => {
     return board;
@@ -27,6 +28,11 @@ const gameBoard = (() => {
   };
 })();
 
+/////////////////
+/////////////////
+///////////////
+///////////////////
+
 const Player = (name, sign) => {
   const getName = () => {
     return name;
@@ -41,6 +47,11 @@ const Player = (name, sign) => {
     getSign,
   };
 };
+
+/////////////////
+/////////////////
+///////////////
+///////////////////
 
 const gameControl = (() => {
   let player1 = Player('Linas', 'X');
@@ -66,6 +77,7 @@ const gameControl = (() => {
 
       if (success) {
         turnNumber++;
+
         checkForWin();
         nextTurn();
       }
@@ -73,6 +85,9 @@ const gameControl = (() => {
   };
 
   const computerMove = () => {
+    minimax();
+
+    //random
     min = Math.ceil(0);
     max = Math.floor(8);
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -84,7 +99,8 @@ const gameControl = (() => {
       : (activePlayer = player2);
 
     if (activePlayer === player2) {
-      move(computerMove());
+      // move(computerMove());
+      minimax();
     }
   };
 
@@ -95,7 +111,110 @@ const gameControl = (() => {
     gameOver = false;
   };
 
-  const checkForWin = () => {
+  const minimax = () => {
+    let availableFields = [];
+    let boardTest = [];
+    let activePlayer = 'O';
+
+    for (let i = 0; i < gameBoard.getBoard().length; i++) {
+      boardTest[i] = gameBoard.getBoard()[i];
+    }
+
+    for (let i = 0; i < boardTest.length; i++) {
+      if (boardTest[i] === '') {
+        availableFields.push(i);
+      }
+    }
+
+    let box = [];
+
+    const run = (state, sign) => {
+      const printState = (board = state) => {
+        console.log('RESULT CHECK FOR:');
+        let line = '';
+        for (let i = 0; i < board.length; i++) {
+          let field = board[i];
+          if (field === '') {
+            field = '#';
+          }
+
+          line = line + field + ' ';
+
+          if (i === 2 || i === 5 || i === 8) {
+            console.log(`${line}\n`);
+            line = '';
+          }
+        }
+      };
+      let scores = [];
+      let states = [];
+
+      const getResult = (state) => {
+        let result = positionChecker(state);
+
+        if (result !== null) {
+          if (result === 'X') {
+            return 10;
+          }
+
+          if (result === 'O') {
+            return -10;
+          }
+
+          if (result === 'tie') {
+            return 0;
+          }
+
+          return null;
+        }
+        console.log(`--- RESULT: ${result}`);
+      };
+
+      printState();
+      let result = getResult(state);
+
+      console.log(result);
+
+      if (result === undefined) {
+      
+        availableFields = [];
+
+        for (let i = 0; i < state.length; i++) {
+          if (state[i] === '') {
+            availableFields.push(i);
+          }
+        }
+
+        console.log('AVAILABLE FIELDS: ' + availableFields);
+
+        availableFields.forEach((field) => {
+          let board = [...state];
+          board[field] = sign;
+          states.push({board, scores});
+        });
+
+        sign === 'O' ? (sign = 'X') : (sign = 'O');
+
+        states.forEach((state) => {
+          let foo = run(state.board, sign);
+
+          console.log(foo);
+          if (foo !== undefined) {
+            state.scores.push(foo);
+            printState(state);
+            console.log(state.scores);
+            console.log(states);
+            box.push(state);
+            console.dir(box);
+          }
+        });
+      }
+      return result;
+    };
+    console.dir(run(boardTest, activePlayer) + "fafafafafafa");
+  };
+
+  const positionChecker = (board = gameBoard.getBoard()) => {
     let winCombos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -107,14 +226,73 @@ const gameControl = (() => {
       [2, 4, 6],
     ];
 
+    let availableFields = [];
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        availableFields.push(i);
+      }
+    }
+
+    let result = null;
+
+    let signs = ['X', 'O'];
+
+    console.log('----------------------------------');
+
+    if (availableFields.length === 0) {
+      result = 'tie';
+    }
+
+    winCombos.forEach((combo) => {
+      signs.forEach((sign) => {
+        let counter = 0;
+        combo.forEach((spot) => {
+          if (board[spot] === sign) {
+            counter++;
+            if (counter === 3) {
+              result = sign;
+              console.log(sign + ': Combo: ' + combo + ', counter:' + counter);
+            }
+          }
+        });
+      });
+    });
+    return result;
+  };
+
+  /////////////////////////////
+  ////////////////////////////
+  ////////////////////////////
+  /////////////////////////////
+  /////////////////////////////
+
+  const runThroughBoard = (board = gameBoard.getBoard()) => {
+    let winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let result = null;
+
+    let sign = activePlayer.getSign();
+
     winCombos.forEach((combo) => {
       let counter = 0;
       combo.forEach((spot) => {
-        if (gameBoard.getBoard()[spot] === activePlayer.getSign()) {
+        // IESKO TIK VIENO SIGN, REIKTU TURBUT, KAD IESKOTU IR X IR O POZICIJO
+        if (board[spot] === sign) {
           counter++;
           if (counter === 3) {
-            displayControl.declareResult(activePlayer);
-            gameOver = true;
+            console.log(
+              'WINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN'
+            );
+            result = activePlayer;
           }
           console.log('Combo: ' + combo + ', counter:' + counter);
         }
@@ -122,13 +300,37 @@ const gameControl = (() => {
     });
 
     if (turnNumber === 9) {
+      result = 'tie';
+    }
+
+    return result;
+  };
+
+  const checkForWin = () => {
+    let result = runThroughBoard();
+
+    if (result !== 'tie' && result !== null) {
+      displayControl.declareResult(activePlayer);
+      gameOver = true;
+    }
+
+    if (result === 'tie') {
       displayControl.declareResult(undefined);
       gameOver = true;
     }
   };
 
-  return { move, reset };
+  return { move, reset, runThroughBoard };
 })();
+
+/////////////////
+/////////////////
+///////////////
+///////////////////
+/////////////////
+/////////////////
+///////////////
+///////////////////
 
 const displayControl = (() => {
   const grid = document.querySelector('.grid');
